3a8ac5d5d94404132bed7161b25154e5497bf1c3
Jonas Meeuws <jonas.meeuws@gmail.com>
2026-03-10 22:31:46 +0100

    fix: Put sending DeathLinks on a cooldown, preventing up to 4 being sent at once on death.

--- Patches/MechaPatches.cs
+++ Patches/MechaPatches.cs
@@ -1,19 +1,25 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace DSP_AP.Patches;
 
 [HarmonyPatch(typeof(Mecha))]
 public class MechaPatches
 {
+    private const double sendDeathLinkCooldown = 10.0d;
+    private static double lastDeathLinkSentTimestamp = 0.0d;
+
     [HarmonyPatch("TakeDamage")]
     [HarmonyPostfix]
     public static void TakeDamagePostfix(Mecha __instance)
     {
         if (__instance.hp <= 0)
         {
-            // The player has died
-            if (Plugin.ArchipelagoClient.DeathLinkHandler != null)
+            // The player is dead
+            double now = Time.realtimeSinceStartupAsDouble;
+            if (Plugin.ArchipelagoClient.DeathLinkHandler != null && now - lastDeathLinkSentTimestamp > sendDeathLinkCooldown)
             {
+                lastDeathLinkSentTimestamp = now;
                 Plugin.ArchipelagoClient.DeathLinkHandler.SendDeathLink("could not handle the Dark Fog.");
             }
         }
